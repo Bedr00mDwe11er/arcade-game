8607f64ab9ef38979cf348532423bd94ab2f4f5e
Matthew Shane Nolan-Cintron <matthewnolancintron@gmail.com>
2018-06-12 20:57:18 -0400

    fix: Move non dynamic content from app.js
    Made a separate css file to handle some of the styling for the character select screen.
    Non dynamic content related to the character select screen is in index.html.

--- js/app.js
+++ js/app.js
@@ -41,12 +41,14 @@ class Enemy {
             //to keep the enemies movements from feeling too much like a loop
             //change it's speed and y position with setSpeedAndYPos method
             this.setSpeedAndYPos(Math.round(Math.random() * 10));
+
         }
 
         // if a collision has been detected
         if (this.collisionDetection()) {
             //call the handleCollision method
             this.handleCollision();
+
         }
 
     }
@@ -61,6 +63,7 @@ class Enemy {
         if (this.x >= (player.x - 45) && this.x <= (player.x + 50) && this.y >= player.y && this.y <= (player.y + 100)) {
             //returning true indicating a collision has occurred
             return true;
+
         }
 
     }
@@ -134,7 +137,7 @@ class Enemy {
 /*The Queen class is used to spawn the enemies*/
 class Queen {
     constructor(allEnemies) {
-      for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 10; i++) {
             //makes a new enemy and adds it to the allEnemies array
             allEnemies.push(new Enemy);
             //sets a semi random speed and y posistion for the most recently made enemy
@@ -162,7 +165,6 @@ class Player {
         /*this property is to prevent the player from moving
         while the character select screen is up*/
         this.isSelectingCharacter = false;
-
     }
 
     // Update the players's position, required method for game
@@ -179,11 +181,12 @@ class Player {
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
-        //TODO: make the score look better
         //displays the player's current score
-        ctx.font = '16px Arial';
+        ctx.font = '1.8em VT323, monospace';
         ctx.fillStyle = '#000';
-        ctx.fillText(`Score:${this.score}`, 0, 500);
+        ctx.fillText(`pts:${this.score}`, 5, 500);
+
+        //TODO: add hearts to the player
     }
 
     /*handles the player's directional movement*/
@@ -200,7 +203,6 @@ class Player {
 
             //log the key that was pressed to the console
             console.log(keyPressed);
-
         }
 
         /*if the left arrow key was pressed and if the players x pos is greater than 0
@@ -273,7 +275,7 @@ class Player {
             setTimeout(() => {
                 this.y = 400;
                 this.x = 200;
-            }, 50);
+            }, 100);
         }
         //if the player was in a collision rest the player with out the delay
         if (collision) {
@@ -283,41 +285,13 @@ class Player {
         }
     }
 
-    /*This method is the character selection screen*/
+    /*This method handle dynamic content for the character selection screen*/
     characterSelect() {
         console.log('choose your character');
 
         /*setting the is selecting character property to true to prevent player
         movments while the character select screen is up*/
         this.isSelectingCharacter = true;
-
-        //create a html element that will sit on top of the game scene
-        let selectScreen = document.createElement('section');
-
-        //create the main heading element for the character select screen
-        const SELECT_SCREEN_HEADING = document.createElement('h1');
-
-        //creating text for the main heading of the select screen
-        const SELECT_SCREEN_HEADING_TEXT = document.createTextNode('Select Your Character');
-
-        //adding text to main heading element
-        SELECT_SCREEN_HEADING.appendChild(SELECT_SCREEN_HEADING_TEXT);
-
-        //add the heading to the select screen
-        selectScreen.appendChild(SELECT_SCREEN_HEADING);
-
-        /*setting styles for the character select screen*/
-        //setting background color
-        selectScreen.style.background = '#abc';
-        //setting the height to be the same as the main game screen, so it covers it.
-        selectScreen.style.height = '606px';
-        //rounding out the corners
-        selectScreen.style.borderRadius = '1%';
-        //placing the character select screen on top of the gamee scene
-        selectScreen.style.marginTop = '100px';
-        selectScreen.style.marginBottom = '-600px';
-        selectScreen.style.position = 'relative';
-        selectScreen.style.zIndex = '1';
 
         /*create an array to store the playable character images
         this will be used to set the src of the image elements
@@ -330,6 +304,15 @@ class Player {
             'images/char-princess-girl.png'
         ];
 
+        /*create an array to store the alt text of the playable character images
+        this will be used to set the alt of the image elements in the character select screen*/
+        const CHARACTER_ALT_TEXT = [
+            'A boy with black hair, beady black eyes, is wearing a orange shirt, black pants and a pair of antenna on his head',
+            'A girl with brown hair, brown eyes, is wearing a green shirt, black pants and a hat with cats ears',
+            'A girl with silver hair, blue eyes, is wearing a black shirt, black pants, and a hat with horns',
+            'A girl with pink hair, green eyes, is wearing a blue shirt, black pants and a white flower pin in her hair '
+        ];
+
         //create an array to store the names of the playable characters
         const CHARACTER_NAMES = [
             'the boy',
@@ -339,99 +322,72 @@ class Player {
             'princess girl'
         ];
 
-        //creating a 'deck' for the 'cards'
-        const CHARACTERS_DECK = document.createElement('ul');
-
-        //will make the cards display horizontally
-        CHARACTERS_DECK.style.display = 'flex';
-        CHARACTERS_DECK.style.flexFlow = 'row wrap';
-
-        //no dots on the cards
-        CHARACTERS_DECK.style.listStyle = 'none';
-
-        //adjusting the placement of the deck
-        CHARACTERS_DECK.style.marginRight = '20px';
-        CHARACTERS_DECK.style.marginLeft = '-20px';
-
-        //create a document FRAGMENT to attach the cards to
-        const FRAGMENT = document.createDocumentFragment();
+        //create a document $fragment to attach the cards to
+        const $fragment = document.createDocumentFragment();
 
         /*cards, images, names and event listeners*/
         for (let c = 0; c < 5; c++) {
             //create a card for each playable character
-            const CHARACTER_CARD = document.createElement('li');
+            const $characterCard = document.createElement('li');
 
             //showing the player the playable characters names
-            CHARACTER_CARD.textContent = `${CHARACTER_NAMES[c]}`;
+            $characterCard.textContent = `${CHARACTER_NAMES[c]}`;
 
-            /*style the card element*/
-            //background color of the cards
-            CHARACTER_CARD.style.background = '#fff';
-            //change the cursor to indicate the card is clickable
-            CHARACTER_CARD.style.cursor = 'pointer';
-            //round the corners
-            CHARACTER_CARD.style.borderRadius = '7%';
-            //add some space around each card
-            CHARACTER_CARD.style.margin = '2%';
-            //size of the cards
-            CHARACTER_CARD.style.flex = '121px';
-            //visual appeal
-            CHARACTER_CARD.style.boxShadow = '10px 10px 10px grey'
+            /*style the card*/
+            //adding a class to handle the cards styles
+            $characterCard.className = 'character-card';
 
             /*if the user is hovering over the card raise it up*/
-            CHARACTER_CARD.addEventListener('mouseover', function() {
+            $characterCard.addEventListener('mouseover', function() {
                 console.log(`you are hovering over ${CHARACTER_NAMES[c]}`);
+
                 //lifting the card
-                CHARACTER_CARD.style.boxShadow = '20px 20px 20px grey';
+                $characterCard.style.boxShadow = '20px 20px 20px grey';
             });
 
             /*when the mouse isn't hovering over the card set it back down*/
-            CHARACTER_CARD.addEventListener('mouseleave', function() {
+            $characterCard.addEventListener('mouseleave', function() {
                 console.log(`you stoped hovering over ${CHARACTER_NAMES[c]}`);
+
                 //setting it back down
-                CHARACTER_CARD.style.boxShadow = '10px 10px 10px grey'
+                $characterCard.style.boxShadow = '10px 10px 10px grey'
             });
 
             /*when the user clicks the cards or selects there character*/
-            CHARACTER_CARD.addEventListener('click',() => {
+            $characterCard.addEventListener('click', () => {
                 console.log(`you selected ${CHARACTER_NAMES[c]}`);
+
                 //set the player's sprite to the character selected
                 this.sprite = `${CHARACTER_SPRITES[c]}`;
 
-                /*setting the is selecting character property to false to allow
+                /*setting the 'isselectingcharacter' property to false to allow
                 for player movment now that character select screen is over*/
                 this.isSelectingCharacter = false;
 
-                /*take the selection screen off of the page and
-                store into variable for later use,
-                I might implement a button or that takes the player back to the
-                character select screen*/
-                selectScreen = document.body.removeChild(selectScreen);
+                /*take the selection screen off of page*/
+                document.querySelector('.select-screen').style.display = 'none';
 
             });
 
             //img element for the character
-            const CHARACTER_IMAGE = document.createElement('img');
+            const $characterImage = document.createElement('img');
 
-            //set the scr of the character image
-            CHARACTER_IMAGE.src = CHARACTER_SPRITES[c];
+            //set the src of the character image
+            $characterImage.src = CHARACTER_SPRITES[c];
+
+            //set the alt of the character images
+            $characterImage.alt = CHARACTER_ALT_TEXT[c];
 
             //add the image to the card
-            CHARACTER_CARD.appendChild(CHARACTER_IMAGE);
+            $characterCard.appendChild($characterImage);
 
-            //add card to the deck
-            CHARACTERS_DECK.appendChild(CHARACTER_CARD);
+            //add the card to the $fragment
+            $fragment.appendChild($characterCard);
 
         }
 
-        //add the deck to the FRAGMENT
-        FRAGMENT.appendChild(CHARACTERS_DECK);
-
-        //add the FRAGMENT to selectScreen
-        selectScreen.appendChild(FRAGMENT);
-
-        //add the character select screen to the body
-        document.body.appendChild(selectScreen);
+        //add the fragment to 'deck'
+        document.querySelector('.characters-deck').appendChild($fragment);
 
     }
 
